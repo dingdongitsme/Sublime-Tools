b26f39dc7dd0f6eb105bdf765721ef35148d663f
dingdongitsme <43582117+dingdongitsme@users.noreply.github.com>
2018-12-15 15:32:31 +0100

    Auto restart
    loops after execution on error

--- Run Rust/runRust.bat
+++ Run Rust/runRust.bat
@@ -1,6 +1,8 @@
 if [%1]==[] (exit /B 1) else (set name=%1) 
+:begin
 cls	
 @echo off
 rustc -A dead_code -A unused_variables %name%
+echo %ERRORLEVEL%
 pause
-exit /B %ERRORLEVEL%
+if %ERRORLEVEL% == 0 (exit %ERRORLEVEL%) else (goto begin)
